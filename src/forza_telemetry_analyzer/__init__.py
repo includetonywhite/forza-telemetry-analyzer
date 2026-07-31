32afb72dc4473720904d4ba3ac5d92b9f67b5efc
--global <include.tony.white@gmail.com>
2026-07-31 12:31:06 -0400

    chore: initialize Python project

---
+++ src/forza_telemetry_analyzer/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from forza-telemetry-analyzer!")
---
+++ tests/test_smoke.py
@@ -0,0 +1,3 @@
+def test_project_is_working() -> None:
+    """A simple test to check if the project is working."""
+    assert 1 + 1 == 2
