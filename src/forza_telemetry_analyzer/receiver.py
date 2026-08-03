c2a0bf987e6e4b8ef224724b66a5f4ddebc3d8c3
--global <include.tony.white@gmail.com>
2026-08-02 23:55:00 -0400

    feat: add UDP receiver

--- src/forza_telemetry_analyzer/receiver.py
+++ src/forza_telemetry_analyzer/receiver.py
@@ -1,20 +1,32 @@
-from _future_ import annotations
+from __future__ import annotations
 
 import socket
+
+from forza_telemetry_analyzer.packet import PacketParseError, parse_packet
 
 HOST = "0.0.0.0"
 PORT = 5300
 BUFFER_SIZE = 2048
 
-Def receive_packets() -> None:
-    "Listen for packets from the Forza Horizon 5 game
+def receive_packets() -> None:
+    "Listen for packets from the Forza Horizon 5 game"
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        s.bind((HOST, PORT))
+        sock.bind((HOST, PORT))
         print(f"Listening for packets on {HOST}:{PORT}")
         while True:
             data, address = sock.recvfrom(BUFFER_SIZE)
+            
+            try:
+                telemetry_packet = parse_packet(data)
+            except PacketParseError as ERROR:
+                print(f"Invalid packet received from {address[0]}: {ERROR}")
+                continue
 
-            print(f"Received {len(data)} bytes packet from {address[0]}: {data[1]}")
+            print(f"{telemetry_packet.timestamep_ms} ms |"
+            # .0f: float to integer the number of decimal places
+            f"RPM: {telemetry_packet.current_engine_rpm:.0f} |"
+            f"Speed X : {telemetry_packet.velocity_x:.2f}"
+            )
         
 if __name__ == "__main__":
     receive_packets()
