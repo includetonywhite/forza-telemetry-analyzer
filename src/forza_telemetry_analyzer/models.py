026b51f8dfbcfd019f83627ad4ef94a8e84ad1f9
--global <include.tony.white@gmail.com>
2026-08-04 21:36:02 -0400

    feat: parse engine and velocity telemetry fields

--- src/forza_telemetry_analyzer/models.py
+++ src/forza_telemetry_analyzer/models.py
@@ -3,9 +3,92 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class TelemetryData:
-    """A dataclass representing a telemetry sample."""
+    is_race_on: int
     timestamp_ms: int
+    engine_max_rpm: float
+    engine_idle_rpm: float
     current_engine_rpm: float
+    acceleration_x: float
+    acceleration_y: float
+    acceleration_z: float
     velocity_x: float
-    throttle: float
-    gear: int
+    velocity_y: float
+    velocity_z: float
+#    angular_velocity_x: float
+#    angular_velocity_y: float
+#    angular_velocity_z: float
+#    yaw: float
+#    pitch: float
+#    roll: float
+#    normalized_suspension_travel_front_left: float
+#    normalized_suspension_travel_front_right: float
+#    normalized_suspension_travel_rear_left: float
+#    normalized_suspension_travel_rear_right: float
+#    tire_slip_ratio_front_left: float
+#    tire_slip_ratio_front_right: float
+#    tire_slip_ratio_rear_left: float
+#    tire_slip_ratio_rear_right: float
+#    wheel_rotation_speed_front_left: float
+#    wheel_rotation_speed_front_right: float
+#    wheel_rotation_speed_rear_left: float
+#    wheel_rotation_speed_rear_right: float
+#    wheel_on_rumble_strip_front_left: int
+#    wheel_on_rumble_strip_front_right: int
+#    wheel_on_rumble_strip_rear_left: int
+#    wheel_on_rumble_strip_rear_right: int
+#    wheel_in_puddle_front_left: int
+#    wheel_in_puddle_front_right: int
+#    wheel_in_puddle_rear_left: int
+#    wheel_in_puddle_rear_right: int
+#    surface_rumble_front_left: float
+#    surface_rumble_front_right: float
+#    surface_rumble_rear_left: float
+#    surface_rumble_rear_right: float
+#    tire_slip_angle_front_left: float
+#    tire_slip_angle_front_right: float
+#    tire_slip_angle_rear_left: float
+#    tire_slip_angle_rear_right: float
+#    tire_combined_slip_front_left: float
+#    tire_combined_slip_front_right: float
+#    tire_combined_slip_rear_left: float
+#    tire_combined_slip_rear_right: float
+#    suspension_travel_meters_front_left: float
+#    suspension_travel_meters_front_right: float
+#    suspension_travel_meters_rear_left: float
+#    suspension_travel_meters_rear_right: float
+#    car_ordinal: int
+#    car_class: int
+#    car_performance_index: int
+#    drivetrain_type: int
+#    num_cylinders: int
+#    car_group: int
+#    smashable_vel_diff: float
+#    smashable_mass: float
+#    position_x: float
+#    position_y: float
+#    position_z: float
+#    speed: float
+#    power: float
+#    torque: float
+#    tire_temp_front_left: float
+#    tire_temp_front_right: float
+#    tire_temp_rear_left: float
+#    tire_temp_rear_right: float
+#    boost: float
+#    fuel: float
+#    distance_traveled: float
+#    best_lap: float
+#    last_lap: float
+#    current_lap: float
+#    current_race_time: float
+#    lap_number: int
+#    race_position: int
+#    accel: int
+#    brake: int
+#    clutch: int
+#    hand_brake: int
+#    gear: int
+#    steer: int
+#    normalized_driving_line: int
+#    normalized_ai_brake_difference: int
+    
--- src/forza_telemetry_analyzer/receiver.py
+++ src/forza_telemetry_analyzer/receiver.py
@@ -23,7 +23,6 @@ def receive_packets() -> None:
                 continue
 
             print(f"{telemetry_packet.timestamp_ms} ms |"
-            # .0f: float to integer the number of decimal places
             f"RPM: {telemetry_packet.current_engine_rpm:.0f} |"
             f"Speed X : {telemetry_packet.velocity_x:.2f}"
             )
